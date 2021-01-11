b3c308450675076988f48f6bb234ab3298fec55d
Karine <harlequinuniform@gmail.com>
2021-01-11 18:54:24 +0100

    Update Draw class with random selection algorithm

--- src/main/java/com/karinedias/model/Draw.java
+++ src/main/java/com/karinedias/model/Draw.java
@@ -3,40 +3,73 @@ package com.karinedias.model;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.math.RoundingMode;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 import java.util.TreeMap;
 
+import org.decimal4j.util.DoubleRounder;
+
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvException;
 
-public class Draw {
+public final class Draw {
+
+	private Draw() {
+
+	}
 
 	/*
 	 * Get a 10 letters random draw based on letters frequencies in EN/FR languages
 	 */
 
-	
-	// méthode pour le tirage
-	public static void getDraw(TreeMap<Character, Double> freq) { 
-		Random r = new Random();
-		List<Character> draw = new ArrayList<>();
-		
-		for (int i = 1; i <= 10; i++) {
-			
-		}
-		
+	/*
+	 * Get 10 letters draw based on 'Roulette Wheel Selection' algorithm
+	 */
+	public static char[] getDraw(Languages lang) throws IOException, CsvException {
+
+		// calling getFrequencies method
+		TreeMap<Character, Double> freq = getFrequencies(lang);
+
 		Random generator = new Random();
-		Object[] values = freq.values().toArray();
-		Object randomValue = values[generator.nextInt(values.length)];
-		
-		System.out.println(randomValue.toString());
-		
+		char[] drawArr = new char[10];
+
+		Double[] cumProb = new Double[26];
+
+		Double sum = 0.0;
+
+		for (int i = 0; i <= freq.size() - 1; i++) {
+
+			Double prevValue = (Double) freq.values().toArray()[i];
+			if (i == 0) {
+				sum = prevValue;
+			} else {
+				sum += prevValue;
+			}
+			cumProb[i] = DoubleRounder.round(sum, 3);
+		}
+
+		Object[] letters = freq.keySet().toArray();
+
+		for (int i = 0; i < drawArr.length; i++) {
+			double random = generator.nextDouble() * cumProb[cumProb.length - 1];
+			int index = Arrays.binarySearch(cumProb, random);
+			if (index < 0) {
+				// Convert negative insertion point to array index.
+				index = Math.abs(index + 1);
+			}
+			drawArr[i] = (char) letters[index];
+
+		}
+		return drawArr;
+
 	}
-	
-	public static TreeMap<Character, Double> getFrequencies (Languages lang) throws IOException, CsvException {
-		
+
+	private static TreeMap<Character, Double> getFrequencies(Languages lang) throws IOException, CsvException {
+
 		ClassLoader classLoader = Draw.class.getClassLoader();
 		InputStream inputStream = classLoader.getResourceAsStream("letter_frequencies.csv");
 
@@ -55,7 +88,7 @@ public class Draw {
 			englishFrequencies.put(strings[1].charAt(0), Double.parseDouble(strings[2]));
 			frenchFrequencies.put(strings[1].charAt(0), Double.parseDouble(strings[3]));
 		}
-		
+
 		switch (lang) {
 		case ENGLISH:
 			return englishFrequencies;
@@ -64,15 +97,21 @@ public class Draw {
 		default:
 			return englishFrequencies;
 		}
-		
+
 	}
 
+	// TEST
 	public static void main(String[] args) throws Throwable, IOException {
 
-
+		// Select language
 		Languages myLanguage = Languages.ENGLISH;
-		
-		getDraw(getFrequencies(myLanguage));
+
+		// Add random generated set of 10 letters
+		char[] myDraw = getDraw(myLanguage);
+
+		for (char c : myDraw) {
+			System.out.print(Character.toUpperCase(c));
+		}
 	}
 
 }
